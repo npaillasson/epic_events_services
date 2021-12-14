1d1c917579f27a4d54099e5486c69303349e1b2c
npaillasson <nathanpaillasson3@gmail.com>
2021-12-14 21:09:48 +0100

    update JWT lifetime

--- src/epic_events_services/settings.py
+++ src/epic_events_services/settings.py
@@ -141,8 +141,8 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(hours=20),
-    "REFRESH_TOKEN_LIFETIME": timedelta(days=5),
+    "ACCESS_TOKEN_LIFETIME": timedelta(hours=2),
+    "REFRESH_TOKEN_LIFETIME": timedelta(hours=4),
     "ROTATE_REFRESH_TOKENS": False,
     "BLACKLIST_AFTER_ROTATION": False,
     "UPDATE_LAST_LOGIN": False,
