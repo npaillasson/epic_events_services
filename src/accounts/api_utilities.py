4adfc26494db96e6f4572a352032ff46a744ce95
npaillasson <nathanpaillasson3@gmail.com>
2021-12-15 18:32:58 +0100

    update partial_user_update in accounts application

--- src/accounts/api_utilities.py
+++ src/accounts/api_utilities.py
@@ -2,6 +2,7 @@ from django.core.exceptions import ObjectDoesNotExist
 from rest_framework.exceptions import NotFound
 from .models import User
 from .validators import custom_password_validator
+from .custom_functions import assignement_is_superuser, assignment_of_groups
 
 
 def get_user(user_id):
@@ -22,6 +23,10 @@ def partial_user_update(serializer, data, obj):
                 serializer.validated_data.get("password")
             )
             obj.set_passord(serializer.password)
+        if key == "team":
+            obj.__dict__[key] = serializer.validated_data.get(key)
+            assignment_of_groups(serializer.validated_data.get(key), obj)
+            obj.is_superuser = assignement_is_superuser(serializer.validated_data.get(key))
         else:
             obj.__dict__[key] = serializer.validated_data.get(key)
     obj.save()
